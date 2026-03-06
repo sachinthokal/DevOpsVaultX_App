c0b635ee5e9fc1352c0079292cc48aa6be6e6b81
Sachin Thokal <sachinthokal1799@gmail.com>
2026-03-07 02:44:02 +0530

    workflow_dispatch added in ci

--- devopsvaultx/settings.py
+++ devopsvaultx/settings.py
@@ -11,7 +11,7 @@ from decouple import config
 # ==================================================
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-test-key-123')
 
 # ==================================================
 # BASIC
